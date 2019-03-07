33d932c0548c1324bcbc34fd5bf7184bc86fad3e
Sammy <samfergot@gmail.com>
2019-03-07 12:36:15 -0500

    tHIS IS MY MEASSAGE

--- src/Game.js
+++ src/Game.js
@@ -17,9 +17,9 @@ new Phaser.Game(config);
 function preload ()
 {
     this.load.setBaseURL('http://labs.phaser.io');
-    this.load.image('chick', 'assets/sprites/chick.png');
-    this.load.image('clown', 'assets/sprites/clown.png');
-    this.load.image('wasp', 'assets/sprites/wasp.png');
+    this.load.image('chick', 'assets/sprites/chick.png');// possible fast class
+    this.load.image('clown', 'assets/sprites/clown.png');// possible sniper class
+    this.load.image('wasp', 'assets/sprites/wasp.png');// possible heavy class
     this.load.image('cursor', 'assets/sprites/drawcursor.png');
     this.load.image('bg', 'assets/skies/fog.png');
 }
@@ -27,7 +27,7 @@ function preload ()
 function create ()
 {
     this.add.image(400, 300, 'bg');
-    this.add.image(1000, 300, 'bg')
+    this.add.image(1000, 300, 'bg');// load background twice to cover entire width of map
     var chicks = this.physics.add.group({key: 'chick', frameQuantity: 3, setXY: { x: 500, y: 400, stepX: 50 }});
     var clown = this.physics.add.image(100, 300, 'clown').setVisible(true);
     var wasp = this.physics.add.image(800, 100, 'wasp').setVisible(true);
@@ -37,8 +37,8 @@ function create ()
     {
         cursor.setVisible(false).setPosition(pointer.x, pointer.y);
 
-        this.physics.moveToObject(clown, pointer, 40);
-        this.physics.moveToObject(wasp, pointer, 80);
+        this.physics.moveToObject(clown, pointer, 40);// last parameter represents speed of class
+        this.physics.moveToObject(wasp, pointer, 20);// heavy class is slowest
 
         Phaser.Utils.Array.Each(
             chicks.getChildren(),
