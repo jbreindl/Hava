b40859dc71f529484e06fefaa5eff8d0e03592f0
brisco17 <44734324+brisco17@users.noreply.github.com>
2019-05-01 02:00:02 -0400

    requirement.txt file

--- src/Game.js
+++ src/Game.js
@@ -58,62 +58,62 @@ function create() {
     }, this);
 
     this.input.keyboard.on('keydown_D', function event() {
-            if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
-                des(this.minnow);
-            }
-            if (this.shark.x < 545) {
-                this.shark.x += 8;
-            }
-        }, this);
+        if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
+            des(this.minnow);
+        }
+        if (this.shark.x < 545) {
+            this.shark.x += 8;
+        }
+    }, this);
 
     this.input.keyboard.on('keydown_W', function event() {
-            if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
-                des(this.minnow);
-            }
-            if (this.shark.y > 20) {
-                this.shark.y -= 8;
-            }
-        }, this);
+        if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
+            des(this.minnow);
+        }
+        if (this.shark.y > 20) {
+            this.shark.y -= 8;
+        }
+    }, this);
 
 //    second sprite
 
     this.input.keyboard.on('keydown_LEFT', function event() {
-            // hit detection event handler
-            if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
-                des(this.minnow);
-            }
-            if (this.minnow.x > 20) {
-                this.minnow.x -= 10;
-            }
-        }, this);
+        // hit detection event handler
+        if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
+            des(this.minnow);
+        }
+        if (this.minnow.x > 20) {
+            this.minnow.x -= 10;
+        }
+    }, this);
 
     this.input.keyboard.on('keydown_DOWN', function event() {
-            // hit detection event handler
-            if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
-                des(this.minnow);
-            }
-            if (this.minnow.y < 491) {
-                this.minnow.y += 10;
-            }
-        }, this);
+        // hit detection event handler
+        if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
+            des(this.minnow);
+        }
+        if (this.minnow.y < 491) {
+            this.minnow.y += 10;
+        }
+    }, this);
 
     this.input.keyboard.on('keydown_RIGHT', function event() {
-            // hit detection event handler
-            if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
-                des(this.minnow);
-            }
-            if (this.minnow.x < 1199) {
-                this.minnow.x += 10;
-            }
-        }, this);
+        // hit detection event handler
+        if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
+            des(this.minnow);
+        }
+        if (this.minnow.x < 1199) {
+            this.minnow.x += 10;
+        }
+    }, this);
 
     this.input.keyboard.on('keydown_UP', function event() {
-            // hit detection event handler
-            if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
-                des(this.minnow);
-            }
-            if (this.minnow.y > 20) {
-                this.minnow.y -= 10;
-            }
-        }, this);
+        // hit detection event handler
+        if (distance(this.minnow.x, this.shark.x, this.minnow.y, this.shark.y) < 50) {
+            des(this.minnow);
+        }
+        if (this.minnow.y > 20) {
+            this.minnow.y -= 10;
+        }
+    }, this);
 }
