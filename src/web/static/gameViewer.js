fbbae3f1557b2a7fda8c9ed831d501ca2973938c
Truong <truongph@buffalo.edu>
2019-05-07 16:52:31 -0400

    gameviewer stuff

--- src/web/static/gameViewer.js
+++ src/web/static/gameViewer.js
@@ -19,6 +19,7 @@ function parseGameState(event) {
 
 
     for (let minnow of gameState['minnows']) {
+        placeMinnow(minnow['x'], minnow['y'], minnow['id'] === socket.id ? '#00ffff' : '#ff00ff')
         placeCircle(minnow['x'], minnow['y'], minnow['id'] === socket.id ? '#00ffff' : '#ff00ff', 2.0);
     }
 
