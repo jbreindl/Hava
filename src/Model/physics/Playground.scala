44617cf96cf124fc4e655be7886cf15eb54f970a
Julius <jbreindl@buffalo.edu>
2019-05-07 16:35:42 -0400

    shark boundary

--- src/Model/physics/Playground.scala
+++ src/Model/physics/Playground.scala
@@ -1,7 +1,7 @@
 package Model.physics
 
 class Playground {
-  var yeetWalls: List[sharkWall] = List()
+  var yeetWalls: List[GridLocation] = List()
   var gridWidth: Int = 30
   var gridHeight: Int = 10
 }
@@ -9,28 +9,28 @@ class Playground {
 object Playground {
     new Playground{
       yeetWalls = List (
-    new sharkWall(0, 29),
-    new sharkWall(1, 29),
-    new sharkWall(2, 29),
-    new sharkWall(3, 29),
-    new sharkWall(4,29),
-    new sharkWall(5,29),
-    new sharkWall(6, 29),
-    new sharkWall(7,29),
-    new sharkWall(8,29),
-    new sharkWall(9, 29),
-    new sharkWall(10,29),
+    new GridLocation(0, 29),
+    new GridLocation(1, 29),
+    new GridLocation(2, 29),
+    new GridLocation(3, 29),
+    new GridLocation(4,29),
+    new GridLocation(5,29),
+    new GridLocation(6, 29),
+    new GridLocation(7,29),
+    new GridLocation(8,29),
+    new GridLocation(9, 29),
+    new GridLocation(10,29),
 
-    new sharkWall(0, 1),
-    new sharkWall(1, 1),
-    new sharkWall(2, 1),
-    new sharkWall(3, 1),
-    new sharkWall(4, 1),
-    new sharkWall(5, 1),
-    new sharkWall(6, 1),
-    new sharkWall(7, 1),
-    new sharkWall(8, 1),
-    new sharkWall(9, 1),
-    new sharkWall(10, 1)
+    new GridLocation(0, 1),
+    new GridLocation(1, 1),
+    new GridLocation(2, 1),
+    new GridLocation(3, 1),
+    new GridLocation(4, 1),
+    new GridLocation(5, 1),
+    new GridLocation(6, 1),
+    new GridLocation(7, 1),
+    new GridLocation(8, 1),
+    new GridLocation(9, 1),
+    new GridLocation(10, 1)
   )
 }}
